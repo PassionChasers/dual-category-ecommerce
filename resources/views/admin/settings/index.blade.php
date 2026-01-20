6efb6bb00a72a82ea11010c34626656c3f06a7f9
Shibu Sharma <168130059+shibusharma1@users.noreply.github.com>
2026-01-20 16:38:23 +0545

    setting

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -172,7 +172,7 @@
 
 @push('scripts')
     {{-- SweetAlert2 CDN --}}
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
